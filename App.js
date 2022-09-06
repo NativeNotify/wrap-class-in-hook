85ae6631b4c7c9ff9c3b19fae1436c72debd9771
pagecow <pagecow@gmail.com>
2022-09-06 12:52:20 -0500

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import registerNNPushToken from 'native-notify';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function wrapClassInHook(Component) {
+  return function WrappedComponent(props) {
+    registerNNPushToken(your-app-id, 'your-app-token');
+    return <Component />
+  }
 }
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+export default wrapClassInHook(App);
 
 const styles = StyleSheet.create({
   container: {
